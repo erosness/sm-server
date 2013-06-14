d14f366003d33f9c6de2f50b9bc54bf293c264e4
Peder Refsnes <peder.refsnes@gmail.com>
2013-06-14 18:45:41 +0200

    discovery: checks if socket is ready to receive before blocking

--- discovery.scm
+++ discovery.scm
@@ -24,19 +24,21 @@
   (udp-join-multicast-group s #f multicast-group #t)
   s)
 
-
-(define (start-discovery port)
+;; (define port 5055)
+(define (start-discovery port heartbeat-interval)
+  (thread-start! (lambda () (heartbeat heartbeat-interval)))
   (define multicast-group "239.255.255.250")
   (print *prefix* "Listening for search requests on " multicast-group ":" port "...")
   (define s (make-multicast-listen-socket "239.255.255.250" port))
   (let loop ()
-      (let-values [((msg addr) (socket-receive-from s 256))]
-        (if (string-ci= "SEARCH" msg)
-            (let ((reply-socket (socket af/inet sock/dgram))
-                  (host (sockaddr-address addr)))
-              (print *prefix* "Sending answer to " host ":" port)
-              (socket-send-to reply-socket "NOTIFY /device\n\nADD VERSION HERE"
-                              (inet-address host port))))
-        )
-      loop
-      (loop)))
+    (if (socket-receive-ready? s)
+        (let-values [((msg addr) (socket-receive-from s 256))]
+          (if (string-ci= "SEARCH" msg)
+              (let ((reply-socket (socket af/inet sock/dgram))
+                    (host (sockaddr-address addr)))
+                (print *prefix* "Sending answer to " host ":" port)
+                (socket-send-to reply-socket heartbeat-packet
+                                (inet-address host port)))))
+        (thread-sleep! 0.005))
+    (loop)))
+;;(start-discovery 5055 360)
