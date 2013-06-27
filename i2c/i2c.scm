b6f9bdebda19f412f5f5fd71f85fb3eb7c751aa8
Kristian Lein-Mathisen <kristianlein@gmail.com>
2013-06-27 14:41:21 +0200

    i2c: adds simple i2c module

---
+++ i2c/i2c.scm
@@ -0,0 +1,27 @@
+(module i2c *
+
+(import chicken scheme foreign)
+(use ioctl posix)
+
+#>
+#include <sys/ioctl.h>
+<#
+
+;; integer typed variant of ioctl (as opposed to the ioctl egg's
+;; pointers)
+(define ioctl-int
+  (foreign-lambda* int ((int fd) (int command) (int arg))
+              "return(ioctl(fd, command, arg));"))
+
+(define (i2c-set-slave-addr fd i2c-addr)
+  ;; taken from linux/i2c-dev.h (not present in android-build system)
+  (define I2C_SLAVE #x0703)
+  (ioctl-int fd I2C_SLAVE i2c-addr))
+
+(define (i2c-open path #!optional (i2c-addr #x34))
+  (define fd (file-open "/dev/i2c-2" open/rdwr))
+  (i2c-set-slave-addr fd i2c-addr)
+  fd)
+
+(define i2c-close file-close)
+)
