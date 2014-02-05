4308d3cf378027d2d1d164299ef03a7fd0a9b936
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-02-05 11:54:32 +0100

    monitor: automatically detect network interface
    and better doc

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,6 +1,10 @@
 
-# listen for cube-server multicasts (search requests and sometimes search
-# responses)
+# listen for cube-server and client search multicasts. will show both
+# requests and hearbeats (not search response, it's not a multicast).
 
-socat -u UDP-RECVFROM:5055,reuseaddr,ip-add-membership=239.255.255.250:10.0.0.32,ip-pktinfo,fork \
+# find the network interface on which we want to listen to (e.g. wlan0)
+iface=`ip r|awk '/default via/ {print $5}'`
+echo "listening on $iface"
+
+socat -u UDP-RECVFROM:5055,reuseaddr,ip-add-membership=239.255.255.250:$iface,ip-pktinfo,fork \
     SYSTEM:"echo \`date +%FT%T\` \$SOCAT_PEERADDR \`cat\`"
