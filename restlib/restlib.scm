8e674c19ab3fc72d18c3eafca23e1a797fda1d2e
Peder Refsnes <peder.refsnes@gmail.com>
2014-02-26 16:51:51 +0100

    restlib: allow body to be empty on PUT/POST requests
    Up till now we assumed that a PUT/POST would always have content. This
    is not the case for calls to play/pause/seek which should be POST's.

--- restlib/restlib.scm
+++ restlib/restlib.scm
@@ -69,13 +69,16 @@
     ;; on GET, call handler with no args. on PUT, call it with parsed
     ;; json input. in both cases, return json-representation of
     ;; handler's return value
+    ;; (current-json) should return false on GET requests and a truthy
+    ;; value for PUT/POST
     (case (request-method (current-request))
       [(GET) (send-json (handler))]
       [(PUT POST)
        (let* ((req-string (request-string!))
               (json (or (read-json req-string)
+                        (string-null? req-string)
                         (error "invalid json" req-string))))
-         (current-json json) ;; <-- never #f
+         (current-json (or json '()))
          (send-json (handler)))]
       (else (error (conc "unsupported method " (request-method (current-request))))))))
 
