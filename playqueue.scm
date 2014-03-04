188b00c64af9a317922b8333a7200d408d774624
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-03-04 17:48:34 +0100

    pq major: removes *pq* globals
    - makes pq record
    - record has fields: list mutex current
    - naming convention on non-thread-safe* versions
    - obs! no tests for thread-safety procs

--- playqueue.scm
+++ playqueue.scm
@@ -1,5 +1,7 @@
 (use srfi-18 uri-common test uuid)
 
+;; (include "concurrent-utils.scm")
+
 ;; the playqueue is the abstractions clients use to manipulate whats
 ;; currently playing. the contents of a playqueue is a playqueue item
 ;; (pq-item); which is an alist with the key 'turi present. when an
@@ -32,74 +34,70 @@
  (test-assert "valid item" (assert-pq-item '((turi . "tr://wimp/play"))))
  (test-error  "wrong key"  (assert-pq-item `((foo . "tr://wimp/play")))))
 
-(define *pq-lock* (make-mutex))
-(define (make-pq) '())
 
-;; lookup functions
-(define (find-with-id id)
-  (find (lambda (elem) (equal? (alist-ref 'id elem) id))
-        *pq*))
+;; ==================== pq record ====================
+(define-record-type pq (%make-pq list mutex current)
+  pq?
+  (list pq-list pq-list-set!)
+  (mutex pq-mutex)
+  (current pq-current pq-current-set!))
 
-;; (define (find-next id)
-;;   (and-let* ((item (find-with-id id)))
-;;             (cadr (memq item *pq* ))))
+(define-record-printer (pq x port)
+  (display (conc "#<pq " (length (pq-list x)) ">") port))
 
-;; (define (find-prev id)
-;;   (and-let* ((item (find-with-id id)))
-;;             (cadr (memq item (reverse *pq*)))))
+(define (make-pq) (%make-pq '() (make-mutex) #f))
 
 
 ;; ****************************
 ;; public api
 
-(define *pq* (make-pq))
-
 ;; The id of the track currently playing, an item with id *pq-current*
 ;; should always be available in the playqueue
-(define *pq-current* #f)
+;; (define *pq-current* #f)
 
-(define pq-add
-  (with-mutex-lock
-   *pq-lock* (lambda (item)
-               (assert-pq-item item)
-               (if (find-with-id (alist-ref 'id item))
-                   item
-                 (let ((item (alist-cons 'id (uuid-v4) item)))
-                   (pp item)
-                   (set! *pq* (add-back *pq* item))
-                   item)))))
 
-(define pq-del
-  (with-mutex-lock
-   *pq-lock* (lambda (id)
-               (and-let* ((item (find-with-id id)))
-                         (set! *pq* (delete item *pq*))))))
+(define (pq-ref* pq item)
+  (let ((id (alist-ref 'id item)))
+    (find (lambda (elem) (equal? (alist-ref 'id elem) id))
+          (pq-list pq))))
 
-(define pq-clear
-  (with-mutex-lock
-   *pq-lock* (lambda ()
-               (set! *pq* (make-pq))
-               (set! *pq-current* #f))))
+(define (pq-add* pq item)
+  (assert-pq-item item)
+  (and (alist-ref 'id item)
+       (error "item already has id field" item))
 
-(begin
-  (pq-clear)
-  (test-assert
-   "play queue empty"
-   (equal? *pq* (make-pq)))
-  (pq-add '((turi . "hoho")))
-  (pq-add '((turi . "haha")))
-  (test-error
-   "will fail since key is wrong" 
-   (pq-add '(wont-be . "added")))
-  (pq-del (alist-ref 'id (car *pq*)))
-  (test-assert
-   "contains one element"
-   (equal? (length *pq*) 1))
-  (pq-clear)
-  (test-assert
-   "play queue is now empty"
-   (equal? *pq* (make-pq))))
+  (let ((item (alist-cons 'id (uuid-v4) item)))
+    (pq-list-set! pq (add-back (pq-list pq) item))
+    item))
 
+(define (pq-del* pq item)
+  (or (pq-ref* pq item) (error "cannot find" item))
+  (pq-list-set! pq (delete item (pq-list pq))))
+
+(define (pq-clear* pq)
+  (pq-list-set! pq '())
+  (pq-current-set! pq #f))
+
+(define (pq-play* pq item)
+  (let* ((item (or (pq-ref* pq item) (error "not found in pq" item)))
+         (track (alist-ref 'turi item))
+         (uuid (alist-ref 'id item)))
+    (play! (play-command track))
+    (print "playing " track)
+    (pq-current-set! pq uuid)))
+
+
+;; ==================== thread-safety ====================
+(define (with-pq-mutex proc)
+  ;; this ain't pretty
+  (lambda (pq . args)
+    ((with-mutex-lock (pq-mutex pq) (lambda () (apply proc (cons pq args)))))))
+
+(define pq-ref   (with-pq-mutex pq-ref*))
+(define pq-add   (with-pq-mutex pq-add*))
+(define pq-del   (with-pq-mutex pq-del*))
+(define pq-clear (with-pq-mutex pq-clear*))
+(define pq-play  (with-pq-mutex pq-play*))
 
 ;; (define pq-next
 ;;   (with-mutex-lock
@@ -114,11 +112,29 @@
 ;;                  (pq-play (alist-ref 'id item))))))
 
 
-(define (pq-play uuid)
-  (with-mutex-lock
-   *pq-lock*
-   (and-let* ((item (find-with-id uuid))
-              (track (alist-ref 'turi item)))
-             (play! (play-command track))
-             (print "playing " track)
-             (set! *pq-current* uuid))))
+
+(test-group
+ "playqueue"
+
+ (let ((pq (make-pq)))
+   (test "initially empty" '() (pq-list pq))
+   (let ((new-item (pq-add* pq `((turi . "a")))))
+     (test "1 item added" (list new-item) (pq-list pq))
+     (pq-del* pq new-item))
+
+   (let ((i1 (pq-add* pq `((turi . "1"))))
+         (i2 (pq-add* pq `((turi . "2")))))
+     (test "2 items: add" (list i1 i2) (pq-list pq))
+     (pq-del* pq i1)
+     (test "1 items: delete" (list i2) (pq-list pq)))
+
+   (pq-add* pq `((turi . "x")))
+   (pq-add* pq `((turi . "y")))
+   (pq-clear* pq)
+   (test "pq-clear" '() (pq-list pq))
+
+   (test-error (pq-add* pq `((id . "hello") (turi . "a"))))
+   (test-error (pq-del* pq `((id . "no existe")))))
+
+
+ (test-error (pq-play* (make-pq) `((id . "a")))))
--- rest-pq.scm
+++ rest-pq.scm
@@ -1,35 +1,41 @@
 (include "playqueue.scm")
 (import broadcast)
 
+(define *pq* (make-pq))
+
 ;; Adds an item to the back of the playqueue and starts it.
 ;; Returns: the passed in item with a unique id added
 (define-handler /pq/play
-  (bc (lambda () (let* ((item (pq-add (current-json))))
-              (pq-play (alist-ref 'id item))
-              item))
+  (bc (lambda ()
+        (let* ((item (current-json))
+               (existing (pq-ref *pq* item))
+               (item (or existing (pq-add *pq* item))))
+          (print "playing " item)
+          (pq-play *pq* item)
+          item))
       "/pq/play"))
 
 ;; Adds an item to the back of the playqueue
 ;; Returns: the passed in item with a unique id added
 (define-handler /pq/add
-  (bc (lambda () (pq-add (current-json))) "/pq/add"))
+  (bc (lambda () (pq-add *pq* (current-json))) "/pq/add"))
 
 
 ;; Removes and item referenced by id from the playqueue
 ;; Does nothing if id is not found in playqueue
 (define-handler /pq/del
   (bc (lambda () (let* ((id (alist-ref 'id (current-json))))
-              (pq-del id)
+              (pq-del *pq* id)
               `((status . "ok"))))
       "/pq/del"))
 
 
 ;; Removes every item from the playqueue and stops the player
 (define-handler /pq/clear
-  (bc (lambda () (pq-clear)
+  (bc (lambda () (pq-clear *pq*)
          (player-quit)
          `((status . "ok")))
       "/pq/clear"))
 
 ;; Returns the playqueue
-(define-handler /pq (lambda () (list->vector *pq*)))
+(define-handler /pq (lambda () (list->vector (pq-list *pq*))))
