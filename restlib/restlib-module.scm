957a0d9b27dbe832240511dfe1488f51bf117b71
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-03-04 13:08:35 +0100

    restlib cleanup: removes s48-modules dependency
    we were thinking of building restlib from outside its root but we're not
    doing that anymore

--- restlib/restlib-module.scm
+++ restlib/restlib-module.scm
@@ -2,7 +2,7 @@
 
 (module restlib *
 (import chicken scheme)
-(use srfi-1 extras data-structures ports s48-modules)
-(include-relative "restlib.scm")
+(use srfi-1 extras data-structures ports)
+(include "restlib.scm")
 
 )
