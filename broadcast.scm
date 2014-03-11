4cdcc6a19e47dea11678b43cea4b65f75290696c
Peder Refsnes <peder.refsnes@gmail.com>
2014-03-11 17:17:23 +0100

    broadcast: adds max size to udp message, ALERT verb
    do avoid dealing with potential problems when udp messages become
    fragmented we set a max size for udp packets
    
    if this size is exceeded we send an ALERT message instead. As opposed
    to NOTIFY messages ALERT has no body. the semantics should be
    something like 'do a get request to the provided path'

--- broadcast.scm
+++ broadcast.scm
@@ -3,6 +3,11 @@
 (import chicken scheme ports data-structures)
 (use socket intarweb spiffy medea)
 
+;; max packet length before the packet might be fragmented
+;; note that this is not necessarily a problem, find out
+;; http://stackoverflow.com/questions/14993000/the-most-reliable-and-efficient-udp-packet-size
+(define max-udp-packet-size 508)
+
 (define (udp-broadcast msg #!optional (saddr (inet-address "239.255.255.250" 5055)))
   (define s (socket af/inet sock/dgram 0))
   (set-socket-option s sol/socket so/broadcast 1)
@@ -15,15 +20,27 @@
         (conc "\n" "Echo: " echo)
         "")))
 
-(define (make-notify path json)
+(define (make-notify path body)
   (conc "NOTIFY "
         path
         (udp-broadcast-headers (current-request))
-        "\n\n" json))
+        "\n\n" body))
+
+(define (make-alert path)
+  (conc "ALERT "
+        path
+        (udp-broadcast-headers (current-request))
+        "\n\n"))
+
+(define (make-udp-message path body)
+  (let ((message (make-notify path body)))
+    (if (< max-udp-packet-size (string-length message))
+        (make-alert path)
+        message)))
 
 (define ((bc proc path) #!rest args)
   (let* ((response (apply proc args))
          (json (with-output-to-string (lambda () (write-json response)))))
-    (udp-broadcast (make-notify path json))
+    (udp-broadcast (make-udp-message path json))
     response))
 )
