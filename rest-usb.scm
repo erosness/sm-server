d53d49289cef51d3bcf80ef7d25b4d32aafd67a1
Peder Refsnes <peder.refsnes@gmail.com>
2014-04-25 16:49:01 +0200

    rest-usb: initial commit of rest interface for usb sticks
    exposes two routes; /search/usb and /browse/usb, both taking q, limit
    and offset parameters.
    
    so far we can only search by song title.
    
    there is also some work remaing for setting db location and the
    mount-point to use as web-root. this should probably be passed in somehow.

---
+++ rest-usb.scm
@@ -0,0 +1,46 @@
+(include "dlna-sql.scm")
+
+(import dlna-sql rest)
+(use spiffy uri-common)
+
+(define *mount-point* "/tmp")
+(define *server-port* 1034)
+
+(set-db! "/tmp/files.db")
+
+(define (urlencode s)
+  (uri-encode-string
+   s
+   (char-set-delete
+    (char-set-complement char-set:uri-unreserved)
+    #\/ #\:)))
+
+; /mount/usb/foo/bar.mp3 -> http://<host>:<port>/foo/bar.mp3
+(define (tid-transform host tid)
+  (let ((base-url (conc "http://" host ":" *server-port*)))
+    (assert (string-prefix? *mount-point* tid)
+            (conc "files outside " *mount-point* " not allowed"))
+
+    (urlencode
+     (conc base-url (string-drop tid (string-length *mount-point*))))))
+
+(define (result-transform result)
+  (let ((host (uri-host (request-uri (current-request)))))
+   (map (lambda (res) (alist-update
+                  'tid
+                  (tid-transform host (alist-ref 'tid res))
+                  res)) result)))
+
+(define ((%make-results proc) q limit offset)
+  (let ((result (result-transform (proc q limit offset))))
+    (make-search-result limit offset (length result) result)))
+
+(define-handler /browse/usb (argumentize (%make-results browse) 'q 'limit 'offset))
+(define-handler /search/usb (argumentize (%make-results search) 'q 'limit 'offset))
+
+(define (start-static-file-server)
+  (root-path *mount-point*)
+  (vhost-map `((".*" . ,(lambda (cont) (cont)))))
+  (start-server port: *server-port*))
+
+;; (define t (thread-start! (lambda () (start-static-file-server))))
