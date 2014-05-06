2d4c8f9f9317f111479131e1fbf61344061369df
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-05-06 18:35:36 +0200

    cli cleanup: make port optional, provide repl

--- cube-server-program.scm
+++ cube-server-program.scm
@@ -1,11 +1,17 @@
+(use srfi-18 matchable)
 
 (include "cube-server.scm")
-
 (include "job-util.scm") ;; job-auto-respawn
+
+(define port (match (command-line-arguments)
+               ((sport) (string->number sport))
+               (else 5055)))
+(define hostname (string-trim-right (with-input-from-pipe "hostname" read-string)))
+
+(warning "started cube-server on http://localhost:" port)
 
 (define dns-sd-unregister! (dns-sd-register (conc hostname "-cube") port))
 
-(if (not (= 1 (length (command-line-arguments))))
-    (error "usage: cube-server <port>"))
-(start-server port: (string->number (car (command-line-arguments))))
+(define server-thread (thread-start! (lambda () (start-server port: port))))
 
+(repl)
