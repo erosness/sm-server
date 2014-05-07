6f319b74c122b63293ab27f3dcb5de122b6ece6a
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-05-07 18:53:00 +0200

    rest extension: adds optional uri argument
    - old API (with default *uris*) is unchanged
    - new API allows specifying which uri-map (so you can have multiple
      start-servers and different handlers)

--- rest.scm
+++ rest.scm
@@ -1,36 +1,52 @@
+;;; use define-handler to map a url to a handler. it also exposes the
+;;; url as a variable so it's easily accessible within your repl.
+;;;
+;;; there can be separate uris (hash tables of urls => handlers)
+;;; specified with optional args. note that if these clash with
+;;; existing top-level variables, they will be written. top-level
+;;; variables should therefore be used with care.
 (module rest (*uris*
               set-handler!
+              get-handler
               wrap-changes
               define-handler
               json-handler)
 
 (import chicken scheme data-structures player broadcast)
-(use srfi-69 ports test uri-common medea multicast spiffy intarweb)
+(use srfi-69 ports test uri-common medea multicast spiffy intarweb test)
 
 ;; ==================== handler ====================
-(define *uris* (make-hash-table))
 
-(define (set-handler! url thunk)
-  (assert (string? url))
-  (hash-table-set! *uris* url thunk))
+(define (set-handler! uris path thunk)
+  (assert (string? path))
+  (hash-table-set! uris path thunk))
 
-(define (find-accessor uri #!optional (uris *uris*))
+(define (get-handler uris uri)
   (hash-table-ref/default uris uri #f))
 
-(define (json-handler)
-  (let ((uri (uri->string (make-uri path: (uri-path (request-uri (current-request)))))))
-    (let ((handler (find-accessor uri)))
+
+;; default uri hash-table
+(define *uris* (make-hash-table))
+
+;; uris: a hash-table of "path" => handler
+(define (json-handler #!optional (uris *uris*))
+  (let ((path (uri->string (make-uri path: (uri-path (request-uri (current-request)))))))
+    (let ((handler (get-handler uris path)))
       (if handler
           (handler)
-          `((error       . ,(conc "not found: " uri))
+          `((error       . ,(conc "not found: " path))
             (valid-urls  . ,(list->vector (hash-table-keys *uris*))))))))
 
 (define-syntax define-handler
   (syntax-rules ()
-    ((define-handler path body ...)
+
+    ((define-handler (path uris) body ...)
      (begin
        (define path body ...)
-       (set-handler! (symbol->string 'path) path)))))
+       (set-handler! uris (symbol->string 'path) path)))
+
+    ((define-handler path body ...)
+     (define-handler (path *uris*) body ...))))
 
 ;; ==================== rest combinators ====================
 
@@ -41,5 +57,14 @@
          (json (with-output-to-string (lambda () (write-json response)))))
     (udp-multicast (change-message path json))
     response))
+
+(test-group
+ "define-handler"
+ (let ((ht (make-hash-table)))
+
+   (define-handler (/testing ht) (lambda () 'ok))
+
+   (test 'ok (/testing))
+   (test 'ok ((get-handler ht "/testing")))))
 
 )
