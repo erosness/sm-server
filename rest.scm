40e0adf1ba24a81218de975b73a4a0302e3a11b0
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-05-13 13:06:11 +0200

    rest util: adds current-host and request-origin
    we're trying to construct URLs back to us. this is surprisingly hard,
    for some reason. but let's hope this is a good start.

--- rest.scm
+++ rest.scm
@@ -4,12 +4,13 @@
               define-handler
               json-handler
               with-request
+              current-host
               start-rest-server!)
 
 (import chicken scheme data-structures player broadcast)
 (use srfi-18 srfi-69 ports
      test uri-common medea multicast spiffy intarweb
-     restlib clojurian-syntax)
+     restlib clojurian-syntax matchable)
 
 (define *server-port* #f)
 ;; ==================== handler ====================
@@ -63,6 +64,69 @@
 
 
 ;; ==================== util ====================
+
+;; try to find out where request came from (the client url that got it
+;; here). this is surprisingly tricky. using host header field and
+;; cleverly composing defaults.
+(define (request-origin request)
+
+  (define (current-port) (uri-port (request-uri request)))
+
+  (match (and request (header-value 'host (request-headers request)))
+    ((host . port) (update-uri (request-uri request)
+                               host: host
+                               port: (or port (current-port)
+                                         *server-port* ;;<-- only applicable in repl
+                                         )))
+    ;; put together some sensible results when request is #f (ie
+    ;; called from repl)
+    (else (make-uri host: "localhost"
+                    port: *server-port*
+                    scheme: 'http
+                    query: '()))))
+
+(test-group
+ "request-origin"
+ (define (str->origin str)
+   (->> str
+        (open-input-string)
+        (read-request)
+        (request-origin)
+        ((lambda (origin) (and origin (uri->string origin))))))
+
+ ;; note: http scheme is missing. spiffy adds these on real
+ ;; requests, though.
+ (test "host + port in header"
+       "//domain:111/"
+       (str->origin "GET / HTTP/1.1\r\nHost: domain:111"))
+ (test "host in header"
+       "//domain/"
+       (str->origin "GET / HTTP/1.1\r\nHost: domain"))
+
+ (test "no host in header"
+       "http://localhost"
+       (str->origin "GET / HTTP/1.1\r\n")))
+
+
+;; pick our current hostname (look up request's host header), or
+;; fallback to localhost (for debugging .. may make it hard to spot
+;; bugs?)
+(define (current-host)
+  (update-uri (request-origin (current-request))
+              path: '(/ "")))
+
+(test-group
+ "current-host"
+
+ (test "host from header value"
+       (uri-reference "//domain.com/")
+       (with-request ("/" `((host ("domain.com" . #f))))
+                     (current-host)))
+
+
+ (test "default host"
+       (uri-reference "http://localhost/")
+       (current-host)))
 
 ;; spawns thread!
 (define (start-rest-server! port)
