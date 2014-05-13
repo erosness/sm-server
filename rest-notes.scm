aca11fa7d58749f16f72929784badfd49d561903
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-05-13 15:11:29 +0200

    rest notes: updates to the new tr://10.0.0.1 scheme

--- rest-notes.scm
+++ rest-notes.scm
@@ -5,30 +5,32 @@
 (use irregex restlib)
 (import rest turi)
 
-(define-handler /search/notes (pagize (argumentize (querify `(((turi . "tr://notes/")))) 'q)))
 
 ;; taken from http://trac.ffmpeg.org/wiki/FancyFilteringExamples
-(define-audio-host "notes"
+(define-turi-adapter note->turi "notes"
   (lambda (uri)
-    (irregex-replace/all
-     "([,;])"
-     (conc
-      "cplay -f lavfi \"aevalsrc="
-      ;; floor(t): 0 0 0 0 0 ... 1 1 1 1 1 ... 2 2 2 2 2
-      ;;  => set a random key when floor(t) changes
-      "if(eq(floor(t),ld(2)),"
-      "st(0,random(4)*3000+1000));"
-      ;; the next value to compare floor(t) with
-      "st(2,floor(t)+1);"
-      ;; mod(t,1) makes t always in the range [0;1) for each key
-      "st(1,mod(t,1));"
-      ;; 0.6*... + 0.4*... for  echo effect
-      ;; exp() to mitigate the sound according to the time
-      "(0.6*sin(1*ld(0)*ld(1))+"
-      "0.4*sin(2*ld(0)*ld(1)))*exp(-4*ld(1))"
-      "\"")
-     (lambda (m) (conc "\\" (irregex-match-substring m 1))))))
+    `((url . ,(irregex-replace/all
+               "([,;])"
+               (conc
+                "aevalsrc="
+                ;; floor(t): 0 0 0 0 0 ... 1 1 1 1 1 ... 2 2 2 2 2
+                ;;  => set a random key when floor(t) changes
+                "if(eq(floor(t),ld(2)),"
+                "st(0,random(4)*3000+1000));"
+                ;; the next value to compare floor(t) with
+                "st(2,floor(t)+1);"
+                ;; mod(t,1) makes t always in the range [0;1) for each key
+                "st(1,mod(t,1));"
+                ;; 0.6*... + 0.4*... for  echo effect
+                ;; exp() to mitigate the sound according to the time
+                "(0.6*sin(1*ld(0)*ld(1))+"
+                "0.4*sin(2*ld(0)*ld(1)))*exp(-4*ld(1))")
+               (lambda (m) (conc "\\" (irregex-match-substring m 1)))))
+      (format . "lavfi"))))
+
+(define-handler /search/notes (pagize (lambda () (list (note->turi 0)))))
+
 )
 
-;; (play! (play-command "tr://notes/"))
+;; (play! (play-command "tr://localhost:5060/t2s?type=notes&id=0"))
 ;; (player-quit)
