32e66ab012e337614da7fcc388e839a6a2237197
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-05-13 14:27:25 +0200

    player: supporting tr://cube-server/t2s redirects
    if a turi is tr://, player will query it for a real stream url

--- player.scm
+++ player.scm
@@ -1,4 +1,20 @@
-(module player (cplay
+;;; player: a simple wrapper around cplay
+;;;
+;;; supports tr:// scheme so that PQ and cube-browser can be on
+;;; separate machine. some tracks, like wimp's tid's, need to
+;;; construct a stream-url based on the track-id. this stream-url is
+;;; valid in 30 minutes and generated just before playback. it is the
+;;; stream-url that is passed to cplay.
+;;;
+;;; if the scheme of the turi is http, it is passed to cplay direcly.
+;;; if it is tr://, that url is used to generate a stream-url.
+;;;
+;;; some tr:// requests actually modify the underlying state. eg.
+;;; tr://localhost/t2s?type=dab&id=57 would tune the DAB to channel 57
+;;; and return a static DAB-url (like
+;;; http://localhost:3345/ffmpeg/ALSA_1_DAB)
+
+(module* player (cplay
                 play!
                 player-pause
                 player-unpause
@@ -6,7 +22,8 @@
                 play-command)
 
 (import chicken scheme data-structures)
-(use fmt test uri-common srfi-18 test)
+(use fmt test uri-common srfi-18 test http-client
+     clojurian-syntax medea)
 
 ;; (include "process-cli.scm")
 ;; (include "concurrent-utils.scm")
@@ -69,11 +86,23 @@
 (define player-quit
   (player-operation #:quit))
 
+
+(define (play-command/tr turi)
+  (->> (with-input-from-request (update-uri turi
+                                            scheme: 'http
+                                            port: (uri-port turi))
+                                #f
+                                read-json)
+       (alist-ref 'url)
+       (cplay)))
+
 (define (play-command turi)
-  ;; uri may be #f if uri-ref can't parse turi
-  ;; TODO: if schema is tr, make rest request and find suri.
-  ;; otherwise, use url directly
-  (error "deprecated"))
+  (let ((turi (if (uri? turi) turi (uri-reference turi))))
+    (case (uri-scheme turi)
+      ((tr) (play-command/tr turi))
+      (else (cplay turi)))))
+
+
 
 (test-group
  "play-command"
@@ -85,5 +114,5 @@
  (test-error (play-command "i l l e g a l")))
 )
 
-;; (define player (play! (play-command "tr://tone/440")))
+;; (define player (play! (play-command "tr://localhost:5060/t2s?type=wimp&id=12345678")))
 ;; (player #:quit)
