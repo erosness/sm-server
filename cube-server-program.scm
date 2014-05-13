787ceae0b45111d0139d99cdab9fce578eb96b48
Peder Refsnes <peder.refsnes@gmail.com>
2014-05-13 19:00:01 +0200

    fix: fixes openssl issue when running compiled binary
    see comment in patch for details

--- cube-server-program.scm
+++ cube-server-program.scm
@@ -1,5 +1,15 @@
 (use srfi-18 matchable)
 
+
+;; FIX for http-client error when doing wimp-login
+;; openssl needs chicken-syntax which is not included by default.
+;; chicken-syntax is also special since it cant be included inside a
+;; module but has to be added at toplevel of the binary we're
+;; producing.
+;; TODO: chicken-syntax is included by default from 4.9.0, remove this
+;; after we've upgraded
+(use chicken-syntax)
+
 (define-values (nickname port)
   (match (command-line-arguments)
     ((name . port) (values name (string->number (optional port "5055"))))
