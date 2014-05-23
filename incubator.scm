e333af840150868f60906a4cc3cce048667c8ee3
Peder Refsnes <peder.refsnes@gmail.com>
2014-05-23 17:34:41 +0200

    alist-merge experimental: allow to be passed in as alist
    we use alist-merge like this:
    
            (alist-merge (pq-current *pq*)
                         (player-pos)
                         (player-paused?))
    
    one or more of these entries might be false, if that's the case, skip
    the alist. this has the nice property of letting us return an empty
    object insted of 'false' if there are no values.
    
    NOTE: this may or may not be a good idea

--- incubator.scm
+++ incubator.scm
@@ -8,8 +8,10 @@
 ;; one alist the latter will be used.
 (define (alist-merge #!rest alists)
   (fold (lambda (alist result)
-          (fold (lambda (item res)
-                  (alist-update (car item) (cdr item) res)) result alist))
+          (if alist
+              (fold (lambda (item res)
+                      (alist-update (car item) (cdr item) res)) result alist)
+              result))
         '() alists))
 
 (test-group
@@ -20,5 +22,7 @@
  (test "works with empty list" '() (alist-merge '()))
  ;; Note that ordering matters when comparing for equality
  (test "introduce new keys" '((baz . 3) (foo . 1) (bar . 2))
-       (alist-merge '((baz . 2) (foo . 1)) '((bar . 2) (foo . 99)) '((baz . 3) (foo . 1)))))
+       (alist-merge '((baz . 2) (foo . 1)) '((bar . 2) (foo . 99)) '((baz . 3) (foo . 1))))
+ (test "alist can be false" '((foo . 1) (bar . 2))
+       (alist-merge '((foo . 1)) #f '((bar . 2)))))
 )
