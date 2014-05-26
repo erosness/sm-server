7a446f4691c284bd2714202e0c783a4fccfc8dbf
Peder Refsnes <peder.refsnes@gmail.com>
2014-05-26 18:01:59 +0200

    cleanup: removes /pos and /pause now that #13 is completed

--- rest-player.scm
+++ rest-player.scm
@@ -108,36 +108,6 @@
 ;; (/player/pq/next)
 ;; (/player/pq/prev)
 
-
-(define-handler /v1/player/pause
-  (wrap-changes "/v1/player/pause"
-                (lambda ()
-                  (and-let* ((parsed (player-paused?)))
-                    (if (current-json)
-                        (let* ((current-state (alist-ref 'value parsed))
-                               (value (alist-ref 'value (current-json))))
-                          (if (not (equal? current-state value))
-                              (if value
-                                  (player-pause)
-                                  (player-unpause)))
-                          `((value . ,value)))
-                        parsed)))))
-
-(define-handler /v1/player/pos
-  (wrap-changes "/v1/player/pos"
-                (lambda ()
-                  (and-let* ((parsed (player-pos)))
-                    (if (current-json)
-                        (let* ((value (alist-ref 'pos (current-json))))
-                          ;; TODO: player-seek should return the
-                          ;; current position after seek instead of
-                          ;; just returning 'ok', faking it for now
-                          (player-seek value)
-                          `((pos . ,value)
-                            (total . ,(alist-ref 'total parsed))))
-                        parsed)))))
-
-
 ;; ==================== seek position hearbeat ====================
 (import broadcast)
 (use looper multicast medea)
