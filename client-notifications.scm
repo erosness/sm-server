17c5d3cd1cb23ef10754566bb9aef4fe8700f1ad
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-06-03 12:07:32 +0200

    client refac: makes client-lib.scm for common func
    we'll be reusing parts of this for our player-rest-test.

--- client-notifications.scm
+++ client-notifications.scm
@@ -7,6 +7,8 @@
 
 (include "incubator.scm")
 (import incubator)
+(include "client-lib.scm")
+
 ;; ==================== repl helpers ====================
 
 (define original-input-port (current-input-port))
@@ -42,49 +44,6 @@
     (liner)))
 
 
-;; ==================== addressing ====================
-
-(define (ip4-address name)
-  (find (lambda (x) (= af/inet (addrinfo-family x)))
-        (address-information name #f)))
-;; (ip4-address "kth.lan")
-
-;; (command-line-arguments '("kth.lan"))
-;; (command-line-arguments '("moo.lan"))
-
-(if (not (= 1 (length (command-line-arguments))))
-    (error "usage: player-address"))
-
-;;  (current-player)
-(define current-player
-  (make-parameter
-   (inet-address
-    (sockaddr-address
-     (addrinfo-address (ip4-address (first (command-line-arguments)))))
-    ;; TODO: take port too from cli
-    5055)))
-
-;; is address our current player? check on IP only (not port, port is
-;; always 5055 for notifications).
-(define (current-player? addr)
-  (equal? (sockaddr-address (current-player))
-          (sockaddr-address addr)))
-
-
-;; get the base REST url for our current server
-(define (current-base-url path)
-  (conc "http://"
-        (sockaddr-address (current-player)) ":"
-        (sockaddr-port (current-player)) "/v1"
-        path))
-;; (current-base-url "/path")
-
-
-(parameterize ((current-player (inet-address "127.0.0.1" 5055)))
-  (test-group
-   "current-base-url"
-   (test "http://127.0.0.1:5055/v1/foo" (current-base-url "/foo"))))
-
 
 ;; ==================== player ====================
 
@@ -92,47 +51,6 @@
 ;; not used (?)
 ;; (define (alist-map proc alist) (map (lambda (pair) (proc (car pair) (cdr pair))) alist))
 
-(begin
-  (define make-notification cons)
-  (define notification-path car)
-  (define notification-value cdr))
-
-
-
-(define (packet->json packet)
-  (assert (string? packet))
-  (define packet-header "NOTIFY ")
-
-  (if (string-prefix? packet-header packet)
-      (values (read-json (string-drop packet (string-length packet-header))))
-      #f))
-;; returns a cons cell with <path> . <json-value> from packet
-(define (json->notification json)
-  (and json
-       (make-notification (alist-ref 'variable json)
-                          (alist-ref 'data json))))
-
-(define (packet->notification packet) (json->notification (packet->json packet)))
-
-(test-group
- "packet->json->notification"
-
- (test "empty json" '() (packet->json "NOTIFY {}"))
- (test "invalid header json" #f (packet->json "notify {}"))
-
- (test "simple json value"
-       `("/path" . ((a . 1)))
-       (json->notification `((variable . "/path")
-                             (data . ((a . 1))))))
-
- (test "empty notification (just to be clear)"
-       (make-notification #f #f)
-       (json->notification '()))
-
- (test "simple packet->notification"
-       `("path" . ((a . 1)))
-       (packet->notification "NOTIFY {\"variable\":\"path\",\"data\":{\"a\":1}}"))
- )
 
 (define (fold-notification n state)
   (alist-update (notification-path n)
@@ -203,31 +121,6 @@
 ;; (pp (urls "http://localhost:5055/_urls"))
 
 
-
-(define (state-volume state)        (get-in state "/v1/player/volume" 'value))
-(define (state-playing state)       (get-in state "/v1/player/current"))
-(define (state-playing-title state) (get-in (state-playing state) 'title))
-(define (state-paused? state)       (get-in (state-playing state) 'paused))
-(define (state-pos state)           (get-in state "/v1/player/pos" 'pos))
-(define (state-total state)         (get-in state "/v1/player/pos" 'duration))
-
-(test-group
- "player state"
-
- (test "Jackson" (state-playing-title
-                  `(("/v1/player/current" .
-                     ((title . "Jackson")
-                      (turi . "tr://10.0.0.29:5055/v1/t2s?type=wimp&id=4124228"))))))
-
- (test #f (state-paused? '(("/v1/player/play" . ((foo . 1))))))
- (test #f (state-paused? '(("/v1/player/play" . ()))))
-
- (test 12 (state-volume `(("/v1/player/volume" . ((value . 12))))))
-
- (let ((state `(("/v1/player/pos" . ((pos . 5) (duration  . 10))))))
-   (test 5  (state-pos state))
-   (test 10 (state-total state))))
-
 (define (state-display state)
   (define (->fx x) (and x (inexact->exact (round x))))
   (conc " ♪" (->fx (state-volume state)) "%"
@@ -236,13 +129,6 @@
         " " (fmt #f (ellipses "..." (trim 40 (dsp (state-playing-title state)))))
         " "
         ))
-
-;; (for-each
-;;  (lambda (fail)
-;;    (let ((expected (car fail))
-;;          (actual (cdr fail)))
-;;      (fmt #t (columnar (dsp expected) "|" (dsp actual)))))
-;;  (filter (lambda (pair) (not (equal/alist? (car pair) (cdr pair)))) (check-state state)))
 
 ;; update server-state based on "packet"
 (define (fold-server-state packet)
