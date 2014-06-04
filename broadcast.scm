f2ddf36c909fa73a3a04a589c4aaa9a5d7734e32
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-06-04 10:00:02 +0200

    cleanup: deletes dead code
    this was left-overs from HTTP NOTIFY days.

--- broadcast.scm
+++ broadcast.scm
@@ -12,15 +12,6 @@
 (import chicken scheme ports data-structures)
 (use socket intarweb spiffy medea multicast)
 
-
-(define (udp-broadcast-headers rq #!optional port)
-  (let ((echo (header-value 'echo (request-headers rq))))
-    (conc
-     (if echo (conc "\nEcho: " echo) "")
-     (if port (conc "\nPort: " (number->string port)) "")
-     "\r\n\r\n")))
-
-
 ;; create a message that represents `path` changing into `body`. port
 ;; specifies "owner" so change-message origins can be identified
 ;; (usually port-number of running service).
@@ -39,11 +30,6 @@
                                           (header-value 'echo (request-headers req))))
                                        (else #f)))))))
 
-;; like change-message, but used for large datasets. tbd
-(define (make-alert path)
-  (conc "ALERT "
-        path
-        (udp-broadcast-headers (or (current-request) (make-request)))))
 
 (include "broadcast.tests.scm")
 )
