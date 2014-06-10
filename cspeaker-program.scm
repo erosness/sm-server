b87bb483ccf80278c6e6ff19b2a287926c90c7e3
Peder Refsnes <peder.refsnes@gmail.com>
2014-06-10 13:08:18 +0200

    cspeaker: adds route for setting/getting player data
    player data will usually consist of a label and iconId, though no
    restrictions are assumed/enforced at this time

--- cspeaker-program.scm
+++ cspeaker-program.scm
@@ -1,5 +1,5 @@
 ;; minimum includes for cspeaker to work
-(use pefat spiffy matchable)
+(use pefat spiffy matchable medea)
 
 
 ;; launch a main program
@@ -10,12 +10,25 @@
 
 ;; does not contain deps (it's included from cube-server.scm too)
 (include "cspeaker.scm")
+(import store)
 
 (print "started cspeaker on http://localhost:" port)
 
 (import rest)
-(define dns-sd-unregister! (dns-sd-register nickname port service-type/cube-pq))
+(define txt-record (string->symbol (conc "dns-" nickname "-" port)))
+(define dns-sd-unregister! (dns-sd-register nickname port service-type/cube-pq txt-record))
 (define server-thread (start-rest-server! port))
 
-(repl*)
+;; ==================== player name and icon ====================
+(define-handler /v1/player
+  (lambda ()
+    (define player-store (make-store txt-record))
+    (if (current-json)
+          (begin
+            (player-store (current-json))
+            (dns-sd-unregister!)
+            (set! dns-sd-unregister!
+                  (dns-sd-register nickname port service-type/cube-pq txt-record))))
+    (or (player-store) '())))
 
+(repl*)
