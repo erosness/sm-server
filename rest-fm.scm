faff6fa7259c3e8aca7e0717cd542f68da9177a0
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-06-27 15:43:45 +0200

    fm major: adds notifications and actual test sine-wave
    - we no longer use turi's to control the catalog via the speaker because
      we decided to try notifications on the catalog directly.
    
    - the actual audio payload mimics a real FM radio but sounds
      horrible (irregular beats)
    
    - needs a bit of cleaning up

--- rest-fm.scm
+++ rest-fm.scm
@@ -1,37 +1,103 @@
-(use test restlib)
+(use test restlib notify clojurian-syntax)
 (import rest)
 
-;; generate the static url for the FM audio payload stream
-(define (fm-suri)
-  `((url . ,(uri->string (update-uri (current-host)
-                                     port: 1101
-                                     path: '(/ "fm"))))
-    ;; if live is #t and the turi is the same as what cplay is
-    ;; currently , we don't need to re-initialize cplay because it'd
-    ;; be playing the very same sound. if live is #f, cplay still
-    ;; needs to be re-initialized because we might want to play from
-    ;; the start.
-    (live . #t)))
+(define (fm-send-audio! khz #!optional (duration 0.5))
+  ;; send a sound snippet to cplay
+  (process-run
+   (conc "ffmpeg -loglevel quiet -f lavfi "
+         " -i \"aevalsrc=sin(" khz "*2*PI*t):d=" duration "\" "
+         " -f s8 udp://localhost:3000?listen")))
 
+;; (fm-send-audio! 1000)
+;; (fm-send-audio! 440 7)
+
+(define *freq* 440)
+
+;; create a thread that keeps pumping audio data to
+;; udp://localhost:3000. if a player isn't listening there, it'll be
+;; ignored. it sounds horrible but it allows testing if your *freq* is
+;; being manipulated properly, and mimics the upcoming HW's model
+;; relatively well.
+(begin
+  (handle-exceptions e (void) (thread-terminate! sa-thread))
+  (define sa-thread
+    (thread-start!
+     (lambda ()
+       (let loop ()
+         (process-wait (fm-send-audio! *freq* 0.1))
+         (thread-sleep! 0.2)
+         (loop))))))
 
 
 ;; ==================== explicit frequency ====================
 
-(define-turi-adapter fmfreq->turi "fmfreq"
+(define-turi-adapter fmfreq->turi "fm"
   (lambda (khz)
-    ;; TODO: tune radio to khz here
-    (fm-suri)))
+    `((url . "udp://localhost:3000")
+      (format . "s8") ;; <-- see "-f s8"
+      ;; if live is #t and the turi is the same as what cplay is
+      ;; currently , we don't need to re-initialize cplay because it'd
+      ;; be playing the very same sound. if live is #f, cplay still
+      ;; needs to be re-initialized because we might want to play from
+      ;; the start.
+      (live . #t))))
+
+(define-handler /v1/catalog/fm/browse
+  (lambda () `((turi . ,(fmfreq->turi "ignored")))))
 
 (define-handler /v1/catalog/fm/seek
   (argumentize (lambda (khz)
                  (cond
 
-                  ((or (equal? khz "up") (equal? khz "down"))
-                   `((turi . ,(fmfreq->turi khz))))
 
-                  ;; construct explicit frequency turis
+                  ((eq? #t khz) `((khz . ,*freq*))) ;; <-- simple getter
+
+                  ;; seek up/down
+                  ((or (equal? khz "up") (equal? khz "down"))
+                   (thread-start! (make-thread
+                                   (->> (lambda ()
+                                          (thread-sleep! 0.5)
+                                          (set! *freq* (+ (if (equal? "up" khz) +1 -1) *freq*)))
+                                        (loop/timeout 5)
+                                        (loop))))
+                   `((status . "success")))
+
+                  ;; seek to specific frequency
                   ((string->number khz) =>
-                   (lambda (khz) `((turi . ,(fmfreq->turi khz)))))
+                   (lambda (khz)
+                     (set! *freq* khz)
+                     `((status . "success"))))
 
                   (else (error "khz must be (number? | up | down)" khz))))
-               'khz))
+               '(khz #t)))
+
+;; ==================== fm (catalog) notifications ====================
+(define *catalog-notify-connections* '())
+(define-handler /v1/catalog/notify
+  (make-notify-handler (getter-with-setter
+                        (lambda () *catalog-notify-connections*)
+                        (lambda (new) (set! *catalog-notify-connections* new)))))
+
+;; do a `curl localhost:5060/v1/catalog/notify` and check with
+;; (send-notification "foobar" `() 0 *catalog-notify-connections*)
+
+(define (fm-thread-iteration)
+  (send-notification "/v1/catalog/fm/seek"
+                     `((khz . ,*freq*))
+                     *server-port*
+                     (getter-with-setter (lambda () *catalog-notify-connections*)
+                                         (lambda (new) (set! *catalog-notify-connections* new)))))
+
+(begin
+  (handle-exceptions e (void) (thread-terminate! fm-thread))
+  (define fm-thread
+    (thread-start!
+     (make-thread
+      (->> (lambda ()
+             (thread-sleep! 1)
+             (fm-thread-iteration))
+           (loop/exceptions (lambda (e) (pp `(error: ,(current-thread)
+                                                ,(condition->list e)))
+                               #f))
+           (loop))
+      "fm-thread"))))
