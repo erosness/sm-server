b3d78a6c9fd1ca32abe6651fb57ce027adadf11b
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-07-15 13:30:45 +0200

    fm bugfix: notifications on delta-only (#45)
    curl localhost:5060/v1/catalog/notify should be pretty quiet now.

--- rest-fm.scm
+++ rest-fm.scm
@@ -82,12 +82,48 @@
 ;; do a `curl localhost:5060/v1/catalog/notify` and check with
 ;; (send-notification "foobar" `() 0 *catalog-notify-connections*)
 
-(define (fm-thread-iteration)
-  (send-notification "/v1/catalog/fm/seek"
-                     `((khz . ,*freq*))
-                     *server-port*
-                     (getter-with-setter (lambda () *catalog-notify-connections*)
-                                         (lambda (new) (set! *catalog-notify-connections* new)))))
+(define (call-when-modified data-thunk thunk)
+  (define last #f)
+  (define (set-last! x) (set! last x))
+  (lambda ()
+    ;; wrap in list so we can tell the difference between never ran
+    ;; and data-thunk returning #f.
+    (let ((check (list (data-thunk))))
+      (if (and last (equal? check last))
+          #t ;; unmodified, keep looping
+          (begin (set-last! check)
+                 (thunk))))))
+
+
+(test
+ "call-when-modified (rest-fm)"
+ "xx"
+ (with-input-from-string "aaaaaaaa"
+   ;; should be run twice: once the first time, then once again on
+   ;; eof.
+   (lambda () (->> (call-when-modified read-char (lambda () (display "x")))
+              (loop/count 1000)
+              (loop)
+              (with-output-to-string)))))
+
+;; repl this and your curl localhost:5060/v1/catalog/notify should trigger:
+;; (set! *freq* (add1 *freq*))
+(define (fm-get-state) `((khz . ,*freq*)))
+
+;; poll-based update mechanism. sends a notification whenever
+;; fm-get-state has changed. see loop/interval below for update
+;; frequency.
+(define fm-thread-iteration
+  (call-when-modified
+   fm-get-state
+   (lambda ()
+     (send-notification "/v1/catalog/fm/seek"
+                        (fm-get-state)
+                        *server-port*
+                        (getter-with-setter (lambda () *catalog-notify-connections*)
+                                            (lambda (new) (set! *catalog-notify-connections* new)))))))
+
+
 
 (begin
   (handle-exceptions e (void) (thread-terminate! fm-thread))
