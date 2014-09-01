78b21907581953ad84cfa166dbca4d5000a2d013
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-09-01 15:03:23 +0200

    amixer hack: add potential support for alsa_amixer on android
    - adds parsers for cget amixer output
    - adds cmdline constructs for our cube-specific kernel (should be temporary!)
    
    replace amixer-volume/cube with amixer-volume/simple to run on your laptop

--- amixer.scm
+++ amixer.scm
@@ -7,6 +7,39 @@
 (import chicken scheme extras)
 (use posix test data-structures irregex)
 
+;; parse the output of amixer cget and cset commands.
+(define (amixer-parse/cget str)
+
+  (let ((regex '(: ":" (* space) "values=" (=> left (+ numeric)) )))
+
+    (cond ((irregex-search regex str) =>
+           (lambda (m)
+             (define (n->num label) (string->number (irregex-match-substring m label)))
+             (n->num 'left)))
+          (else (error "could not parse" str)))))
+
+(test-group
+ "amixer-parse/cget"
+
+ (test "simplest amixer case" '0 (amixer-parse/cget "\n  : values=0,0"))
+
+ (test "amixer result parser"
+       10
+       (amixer-parse/cget
+        "numid=3,iface=MIXER,name='Master Playback Volume'
+  ; type=INTEGER,access=rw------,values=2,min=0,max=65536,step=1
+  : values=10,10\n"))
+
+
+ (test "alsa_amixer android"
+       13
+       (amixer-parse/cset
+        "numid=110,iface=MIXER,name='Playback Volume (plain)'
+  ; type=INTEGER,access=rw---R--,values=1,min=0,max=8388608,step=0
+  : values=13
+  | dBscale-min=-80.00dB,step=0.80dB,mute=0
+")))
+
 
 ;; parse simple-control get output
 (define (amixer-parse/sget str)
@@ -35,7 +68,28 @@
   Front Right: Playback 13108 [20%] [on]
 ")))
 
-(define (amixer-volume #!optional (value #f))
+
+
+;; TODO: deprecate this once simple controls become available on Cube.
+;; we want to use the same API for both.
+(define (amixer-volume/cube #!optional value)
+
+  (let* ((scale (/ 65500 100))
+         (output (if value
+                     ;; set volume
+                     (with-input-from-pipe
+                      (conc "alsa_amixer -c 1 cset name=\"Playback Volume (plain)\" "
+                            (* value scale))
+                      read-string)
+                     ;; get volume
+                     (with-input-from-pipe
+                      "alsa_amixer -c 1 cget name=\"Playback Volume (plain)\" "
+                      read-string))))
+
+    (/ (amixer-parse/cget output) scale)))
+
+
+(define (amixer-volume/simple #!optional (value #f))
 
   (if (and value (not (number? value))) (error "invalid volume " value))
 
@@ -44,4 +98,7 @@
                     (with-input-from-pipe "amixer sget Master Playback" read-string))))
 
     (amixer-parse/sget output)))
+
+(define amixer-volume amixer-volume/cube)
+
 )
