ccc30bc3e08eafca3d7ff691cd524451e1a561fb
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-09-01 13:42:45 +0200

    repl bugfix: use corrent repl server-port
    we need to initialize the server-port parameter before starting the
    server now.

--- cube-server-repl.scm
+++ cube-server-repl.scm
@@ -3,8 +3,11 @@
 
 (include "cube-server.scm")
 
-(define dns-sd-unregister!/browser (dns-sd-register "repl" 5060 service-type/cube-browser))
-(define dns-sd-unregister!/pq      (register-pq-with-icon-store "repl" 5060))
+(define port 5060)
+(server-port port)
+
+(define dns-sd-unregister!/browser (dns-sd-register "repl" port service-type/cube-browser))
+(define dns-sd-unregister!/pq      (register-pq-with-icon-store "repl" port))
 (define server-thread (start-rest-server!))
 
 (import rest-wimp)
