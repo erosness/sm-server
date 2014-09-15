2579587d2f7e3718548540d7974552184e6dae7f
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-09-15 15:57:19 +0200

    dab sync: introduce synchronous dab-command
    because dab-send with dab-read-thread was bad and confusing.
    
    note that dab-command is not yet used

--- dab/dab-i2c.scm
+++ dab/dab-i2c.scm
@@ -1,15 +1,18 @@
 (module dab-i2c *
-(import extras chicken scheme)
+(import extras chicken scheme data-structures)
 
-(use dab srfi-18 bitstring dab i2c posix)
+(use dab srfi-18 bitstring dab i2c posix srfi-13
+     srfi-14 ;; char-set
+     matchable
+     )
 
 (define current-dab-fd
   (set-finalizer! (let ((fd (i2c-open "/dev/i2c-2" #x75)))
                     (lambda () fd))
                   (lambda (get-fd) (file-close (get-fd)))))
 
-;; Add 2-byte "length" field, see FSAPI protocol reference API section
-;; 4.3.3 on SCB framing
+;; Send a raw dab packet. Add 2-byte "length" field, see FSAPI
+;; protocol reference API section 4.3.3 on SCB framing
 (define (dab-send-packet bs)
   (let* ((len (/ (bitstring-length bs) 8))
          (zbs (bitconstruct (len 16) (bs bitstring))))
@@ -30,25 +33,47 @@
     (current-frame (add1 (current-frame)))
     response))
 
+;; if we forget to read responses after sending commands, we'll have
+;; unexpected responses buffered. this should clear those. but it
+;; should never be necessary!
+(define (dab-flush-input)
+  (let loop ()
+    (if (string-null? (dab-read-packet))
+        (void)
+        (loop))))
 
-;; ==============================
-;; repl helper that prints incoming frames
-(begin
-  (handle-exceptions e (void) (thread-terminate! dab-read-thread))
-  (define dab-read-thread
-    (thread-start!
-     (lambda ()
-       (let loop ()
-         (or
-          (let ((packet (dab-read-packet)))
-            (if (string-null? packet)
-                ;; sleep a little if we didn't get any packets. if we did, try
-                ;; reading the next one immediately
-                (thread-sleep! 0.1)
-                ;; only print if packet isn't empty
-                (pp (parse-frame packet)))))
-         (loop))))))
+;; synchronous version of dab-send-packet and friends. should be
+;; thread-safe too (important because of dab-refresh-channels!)
+(define dab-command
+  (let ()
+    (define mutex (make-mutex 'dab-command))
+    (lambda (bs)
+      (handle-exceptions e
+        (raise e)
+        (dynamic-wind
+          (lambda () (mutex-lock! mutex))
+          (lambda ()
+            (define fid-sent (current-frame))
+            (dab-flush-input)
+            (dab-send bs)
+            (let loop ()
+              ;; try to not block other srfi-13 threads
+              (thread-wait-for-i/o! (current-dab-fd))
+              (let ((response (dab-read-packet)))
+                (if (string-null? response)
+                    (loop)
+                    (let ((frame (parse-frame response)))
+                      (match frame
+                        (('frame fid reply)
+                         (if (not (= fid fid-sent))
+                             (error (conc "invalid reply frame for fid " fid-sent)
+                                    frame))
+                         reply)
+                        (else (error "invalid DAB frame" frame))))))))
+          (lambda () (mutex-unlock! mutex)))))))
 
+;; looks ok:
+;; (pp (map thread-join! (list-tabulate 10 (lambda (idx) (thread-start! (lambda () (thread-yield!) (dab-command (dab.tuneStatus))))))))
 
 
 ;; before any module can be turned on (eg dab.state), all the other
