e4977ae8fd19fb8bc1eb4dc9707e82e5a323748d
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-09-17 14:30:21 +0200

    amixer notify: adds notify messages to volume/eq changes
    this should sync all the app volume/eq sliders (like the old
    simple-wrappers did)

--- rest-audio.scm
+++ rest-audio.scm
@@ -2,6 +2,7 @@
 (include "amixer.scm")
 
 (import rest)
+(import notify)
 (import amixer)
 
 (define mute      #f)
@@ -34,6 +35,41 @@
 
 (define-simple-wrapper /v1/player/mute   mute      boolean?)
 
+;; return-value of setter is used as notification payload. setter and
+;; notification is only called if the new value is != old value.
+;; returns a procedure which, if called with 1 argument, works like a
+;; normal cache. if called with 0 arguments, uses getter to fetch the
+;; latest value and uses that as the "new value".
+;;
+;; the returned procedure can be called regularly without arguments to
+;; keep send notifications based on changed, polling for changes on
+;; whatever getter returns.
+(define (with-notification path getter setter)
+  ;; last if wrapped in list so we can distinguish unset values and #f
+  ;; from getter.
+  (define last #f)
+  (define (update x) (set! last (list x)))
+  (define (changed? y) (not (equal? (list y) last)))
+  (lambda (#!optional (nval (getter)))
+    (if (changed? nval)
+        (begin
+          (send-notification path (setter nval))
+          (set! last (list nval))))
+    nval))
+
+;; calling /notify versions without arguments will query amixer for
+;; current volume and notify if changed. calling it with argument,
+;; will set that value using amixer, cache it for later comparison,
+;; and send notify messages to everybody.
+(define amixer-volume/notify
+  (with-notification "/v1/player/volume" amixer-volume
+                     (lambda (volume)
+                       `((value . ,(amixer-volume volume))))))
+
+(define amixer-eq/notify
+  (with-notification "/v1/player/eq" amixer-eq
+                     (lambda (eqlst)
+                       `((value . ,(list->vector (amixer-eq eqlst)))))))
 
 ;; HACK: use mock volume and eq on macosx
 (cond-expand
@@ -43,8 +79,8 @@
   (define-handler /v1/player/volume
     (lambda ()
       (let ((vol (if (current-json)
-                     (amixer-volume (alist-ref 'value (current-json)))
-                     (amixer-volume))))
+                     (amixer-volume/notify (alist-ref 'value (current-json)))
+                     (amixer-volume/notify))))
         `((value . ,vol)))))))
 
 
@@ -56,6 +92,6 @@
   (define-handler /v1/player/eq
     (lambda ()
       (let ((eq (if (current-json)
-                    (amixer-eq (vector->list (alist-ref 'value (current-json))))
-                    (amixer-eq))))
+                    (amixer-eq/notify (vector->list (alist-ref 'value (current-json))))
+                    (amixer-eq/notify))))
         `((value . ,(list->vector eq))))))))
