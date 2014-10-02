erosness/sm-server

commit 044d8d10a8c5ab185965512486b04c368b3c8876

amixer bugfix: don't set volume on external changes
- if volume was changed by argument (presumable from REST call), call
  alsa_amixer sset to reflect new volume on hw.

- if volume was changed by getter, it was modified externally. send
  notification only, don't make any more alsa_amixer calls (as before
  this patch).

rest-audio.scm
@@ -43,31 +43,42 @@
 ;; the returned procedure can be called regularly without arguments to
 ;; keep send notifications based on changed, polling for changes on
 ;; whatever getter returns.
-(define (with-notification path getter setter)
+(define (with-notification path getter setter value->notification)
   ;; last if wrapped in list so we can distinguish unset values and #f
   ;; from getter.
   (define last #f)
   (define (changed? y) (not (equal? (list y) last)))
-  (lambda (#!optional (nval (getter)))
-    (if (changed? nval)
-        (begin
-          (send-notification path (setter nval))
-          (set! last (list nval))))
-    nval))
+  (define (change! y) (set! last (list y)))
+
+  ;; if new-value ≠ old-value, broadcast change and cache new-value
+  (define (notify!? new-value)
+    (cond ((changed? new-value)
+           (send-notification path (value->notification new-value))
+           (change! new-value))))
+
+  (lambda (#!optional nval)
+    (value->notification
+     (cond (nval (notify!? (setter nval)) nval) ;; change by arg
+           (else (let ((n (getter)))            ;; change externally
+                   (notify!? n)
+                   n))))))
 
 ;; calling /notify versions without arguments will query amixer for
 ;; current volume and notify if changed. calling it with argument,
 ;; will set that value using amixer, cache it for later comparison,
 ;; and send notify messages to everybody.
 (define amixer-volume/notify
-  (with-notification "/v1/player/volume" (lambda () (amixer-volume))
-                     (lambda (volume)
-                       `((value . ,(amixer-volume volume))))))
+  (with-notification "/v1/player/volume"
+                     (lambda () (amixer-volume))
+                     (lambda (v) (amixer-volume v))
+                     (lambda (v) `((value . ,v)))))
+
 
 (define amixer-eq/notify
-  (with-notification "/v1/player/eq" amixer-eq
-                     (lambda (eqlst)
-                       `((value . ,(list->vector (amixer-eq eqlst)))))))
+  (with-notification "/v1/player/eq"
+                     (lambda () (amixer-eq))
+                     (lambda (eq) (amixer-eq eq))
+                     (lambda (eq) `((value . ,(list->vector eq))))))
 
 ;; HACK: use mock volume and eq on macosx
 (cond-expand

commit 3485d849440f3c9c4ec380c6947db5985a8b3370

rest-audio bugfix: returned json wrapped twice
this is embarrasing. the JSON api broke in 044d8d10a. before this patch:

    $ curl linux.local:5055/v1/player/volume
    {"value":{"value":50}}
    $ curl linux.local:5055/v1/player/eq
    don't know how to write datum as JSON: (((value . #(50 50 50 50 50))))

after this patch:

    $ curl linux.local:5055/v1/player/volume
    {"value":50}
    $ curl linux.local:5055/v1/player/eq
    {"value":[50,50,50,50,50]}

rest-audio.scm
@@ -87,10 +87,9 @@
  (else
   (define-handler /v1/player/volume
     (lambda ()
-      (let ((vol (if (current-json)
+      (if (current-json)
                      (amixer-volume/notify (alist-ref 'value (current-json)))
-                     (amixer-volume/notify))))
-        `((value . ,vol)))))))
+          (amixer-volume/notify))))))
 
 
 (cond-expand
@@ -100,10 +99,10 @@
  (else
   (define-handler /v1/player/eq
     (lambda ()
-      (let ((eq (if (current-json)
+      (if (current-json)
                     (amixer-eq/notify (vector->list (alist-ref 'value (current-json))))
-                    (amixer-eq/notify))))
-        `((value . ,(list->vector eq))))))))
+          (amixer-eq/notify))))))
+
 
 ;; volume watchdog thread. if the volume is modified externally
 ;; (hardware volume button, for example), we pick it up in cube-server