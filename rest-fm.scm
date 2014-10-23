e5f36ce8bc631b9d90e2b81840df31b20d18d4be
Peder Refsnes <peder.refsnes@gmail.com>
2014-10-23 18:30:27 +0200

    rest,fm,wip: initial implementation of rest api
    Initial version has two routes; SEEK and NOTIFY
    
    SEEK /v1/catalog/fm/seek
    
    commands for controlling the radio is done through the hz query
    parameter. you might think that sinze it's called hz it takes the
    desired frequency in hertz as a value. if so, you would only be
    partially right.
    
    supported values:
    
    none: return a snapshop of the current state (see fields included below)
    number: try to tune the radio to this frequency, if it's in the range
            87500 to 108000 it will most likely succeed, if not in range it
            does nothing
    "up": starts a search upward with wrap around
    "down": starts a search downward with wrap around
    
    SEEK also supports the following values which may be redundant
    
    "step-up":   sets frequenzy to current + step size (currently 100)
    "step-down": same
    
    -----
    
    NOTIFY /v1/catalog/fm/notify
    
    the current state of the radio is broadcasted on the notify route
    whenever the radio state changes. radio state currently entails the
    includes the following fields:
    
     - title: the current frequency
     - tuneStatus: 'idle (we're searching), 'decoding (we're broadcasting audio)
     - subtitle: channel name or empty string
     - signal strength
    
    the turi is currently removed from notifications sinces it outputs
    localhost as hostname.

--- rest-fm.scm
+++ rest-fm.scm
@@ -1,13 +1,18 @@
+(use test restlib clojurian-syntax dab-i2c looper)
+(import rest notify turi)
 
 ;; ==================== fm (catalog) notifications ====================
 (define *catalog-notify-connections* '())
-(define-handler /v1/catalog/notify
+
+(define-handler /v1/catalog/fm/notify
   (make-notify-handler (getter-with-setter
                         (lambda () *catalog-notify-connections*)
                         (lambda (new) (set! *catalog-notify-connections* new)))))
 
 ;; do a `curl localhost:5060/v1/catalog/notify` and check with
-;; (send-notification "foobar" `() *catalog-notify-connections*)
+;; (send-notification "foobar" '((yo . "yo")) (getter-with-setter
+;;                                  (lambda () *catalog-notify-connections*)
+;;                                  (lambda (new) (set! *catalog-notify-connections* new))))
 
 
 (define (call-when-modified data-thunk thunk #!optional (pred (constantly #t)))
@@ -39,9 +44,28 @@
               (loop)
               (with-output-to-string)))))
 
-;; repl this and your curl localhost:5060/v1/catalog/notify should trigger:
-;; (set! *freq* (add1 *freq*))
-(define (fm-get-state) `((khz . ,*freq*)))
+
+(define (turi-command params)
+  (or (and-let* ((hz (alist-ref 'hz params))
+                 (val (string->number hz)))
+        (ensure-fm-on)
+        (fm-frequency val)
+        ;; TODO: find IP so zones can reach DAB
+        `((url . "http://127.0.0.1:8090/dab/hi")))
+      (error "invalid fm params. expected hz key with number value")))
+
+(define-turi-adapter fmfreq->turi "fm"
+  turi-command)
+
+(define (fm-get-state)
+  (let* ((freq (fm-frequency))
+         (turi-alist `((hz . ,freq))))
+   `((title . ,freq)
+     (tuneStatus . ,(symbol->string (parse-dab.tuneStatus (dab-command (fm.tuneStatus)))))
+     (subtitle . ,(or (fm-radio-text) ""))
+     (turi . ,(fmfreq->turi turi-alist))
+     (signalStrength . ,(fm-signal-strength)))))
+
 
 ;; poll-based update mechanism. sends a notification whenever
 ;; fm-get-state has changed. see loop/interval below for update
@@ -51,11 +75,14 @@
    fm-get-state
    (lambda ()
      (send-notification "/v1/catalog/fm/seek"
-                        (fm-get-state)
+                        ;; TODO: turi host always resolves to localhost
+                        ;; here, since we don't have a current request
+                        ;; to look at. Remove it to avoid confusion.
+                        (alist-delete 'turi (fm-get-state))
                         (getter-with-setter (lambda () *catalog-notify-connections*)
-                                            (lambda (new) (set! *catalog-notify-connections* new)))))))
-
-
+                                            (lambda (new) (set! *catalog-notify-connections* new)))))
+   ;; only query fm frequency if we have listeners
+   (lambda () (pair? *catalog-notify-connections*))))
 
 (begin
   (handle-exceptions e (void) (thread-terminate! fm-thread))
@@ -69,3 +96,41 @@
                                #t))
            (loop))
       "fm-thread"))))
+;;(thread-terminate! fm-thread)
+
+(define (ensure-fm-on)
+  (if (not (fm-on?)) (fm-turn-on)))
+
+(define fm-step-size 100)
+
+(define-handler /v1/catalog/fm/seek
+  (argumentize (lambda (hz)
+                 (ensure-fm-on)
+                 (cond
+                  ((eq? hz #t)
+                   (fm-get-state))
+
+                  ((equal? "up" hz)
+                   (dab-command (fm.search 'up))
+                   `((status . "ok")))
+
+                  ((equal? "down" hz)
+                   (dab-command (fm.search 'down))
+                   `((status . "ok")))
+
+                  ((equal? "step-up" hz)
+                   (let ((freq (fm-frequency)))
+                     (fm-frequency (+ freq fm-step-size))
+                     `((status . "ok"))))
+
+                  ((equal? "step-down" hz)
+                   (let ((freq (fm-frequency)))
+                     (fm-frequency (- freq fm-step-size))
+                     `((status . "ok"))))
+
+                  ((string->number hz) =>
+                   (lambda (hz)
+                     (fm-frequency hz)
+                     `((status . "ok"))))))
+               '(hz #t)))
+
