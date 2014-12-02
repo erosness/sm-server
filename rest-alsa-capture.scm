f656d4cb248297b0f51d0a14d21ba282970d7c1c
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-12-02 18:06:44 +0100

    bt: no ffserver, use direct audio source

--- rest-alsa-capture.scm
+++ rest-alsa-capture.scm
@@ -1,14 +1,22 @@
+;;; $ adb shell alsa_arecord -L | grep CARD
+;;; default:CARD=imxspdif
+;;; default:CARD=imxaudiovenice9
+;;; default:CARD=imxaudiobtm720
+;;; default:CARD=imxaudiotr1
+;;;
+;;; OBS: venice9 is "slightly" more complex and used in rest-dab.scm
 (use restlib)
 
 (import turi)
 
-(define-turi-adapter bt->turi "bt"
+(define-turi-adapter bt->turi "alsa"
   (lambda (params)
-    `((url . "http://127.0.0.1:8090/bt"))))
+    `((url . ,(alist-ref 'd params))
+      (format . "alsa"))))
 
 (define-handler /v1/catalog/bt
   (lambda ()
-    `((turi . ,(bt->turi '()))
+    `((turi . ,(bt->turi '((d . "default:CARD=imxaudiobtm720"))))
       (title . "Bluetooth")
       (type . "bt")
       (image . "http://www.kirya.net/wp-content/uploads/2007/07/bluetooth_logo.png"))))
