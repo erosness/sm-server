d56fbe136d8dd78c2e5bd8157bc4dbd6066fe431
Peder Refsnes <peder.refsnes@gmail.com>
2015-04-20 14:53:27 +0200

    rest usb rewrite: big update to rest interface
    - encoding: changes to a very conservative escape scheme; escape
      everything except a-z A-z 0-9. This makes handling corrupted filenames
      etc. more robust
    
    - rewrite duration from db to match semantics expected by clients. in
      other words output duration in seconds.
    
    - add top level catalog url; /v1/catalog/usb

--- rest-usb.scm
+++ rest-usb.scm
@@ -1,41 +1,157 @@
 (include "dlna-sql.scm")
 
 (import dlna-sql)
-(use spiffy uri-common intarweb restlib)
+(use spiffy uri-common intarweb restlib regex test
+     clojurian-syntax srfi-14)
 
-(define *mount-point* "/tmp")
-(define *static-server-port* 1034)
+;; Static file server
+; assume a static file server is running, serving files from
+; *web-root* on port *static-server-port*.
 
-(set-db! "/tmp/files.db")
+;; Port where static file server is listening
+(define *static-server-port* 5057)
 
-(define (urlencode s)
+(cond-expand
+ ((not arm)
+  ;; Location in filesystem where USB stick is mounted
+  (define *web-root* "/tmp")
+  ;; Path to check for database file
+  (set-db! "/tmp/mediadb/files.db"))
+ (else
+  ;; TODO: what paths should be use?
+  (define *web-root* "/data")
+  (set-db! "/data/files.db")
+  ))
+
+;; URL encoding
+
+;; We're serving up arbitrary audio files here, so their names can be anything.
+;; Escape everything except [A-Za-z0-9.]
+(define char-set:a-z (ucs-range->char-set (char->integer #\a)
+                                          (char->integer #\z)))
+
+(define char-set:A-Z (ucs-range->char-set (char->integer #\A)
+                                          (char->integer #\Z)))
+(define (url-encode s)
   (uri-encode-string
    s
-   (char-set-delete
-    (char-set-complement char-set:uri-unreserved)
-    #\/ #\:)))
+   (char-set-complement
+    (char-set-union char-set:a-z
+                    char-set:A-Z
+                    char-set:digit
+                    (char-set #\.)))))
 
-; /mount/usb/foo/bar.mp3 -> http://<host>:<port>/foo/bar.mp3
-(define (tid-transform host tid)
-  (let ((base-url (conc "http://" host ":" *static-server-port*)))
-    (assert (string-prefix? *mount-point* tid)
-            (conc "files outside " *mount-point* " not allowed"))
+(test-group
+ "url-encoding"
+ (test "ascii letters and digits are not encoded"
+       "foobar123" (url-encode "foobar123"))
+ (test "non a-z A-Z 0-9 chars are encoded"
+  "%C3%A6%C3%B8%C3%A53a%CC%8A" (url-encode "æøå3å"))
+ (test "" (url-encode "")))
 
-    (urlencode
-     (conc base-url (string-drop tid (string-length *mount-point*))))))
+;;;
 
-(define (result-transform result)
-  (let ((host (uri-host (request-uri (current-request)))))
-   (map (lambda (res) (alist-update
-                  'tid
-                  (tid-transform host (alist-ref 'tid res))
-                  res)) result)))
+;; TODO: create global debug logger
+(define (dbg . args)
+  (print "DEBUG: " (apply conc args)))
+
+;;;
+
+(define (transform-duration duration-str)
+  ;; INPUT:   Duration string (like "H:M:S.MS")
+  ;; RETURNS: Duration in seconds or #f
+
+  (and-let* (;; Split on separators -> list
+         (fields (string-split-fields "[:.]" duration-str #:infix))
+         ;; Assert we got at least 3 fields
+         ((<= 3 (length fields)))
+         ;; convert to number
+         (fields (map string->number fields)))
+
+    ;; Multiply hours, minutes and seconds to obtain
+    ;; duration in seconds
+    (fold + 0 (map * '(3600 60 1) fields))))
+
+(test-group
+ "transform-duration"
+ (test 142 (transform-duration "0:02:22.933"))
+ ;; (+ (* 3600 1) (* 60 34) 12) => 5652
+ (test (+ (* 3600 1) (* 60 34) 12)
+       (transform-duration "01:34:12"))
+ (test #f (transform-duration "invalid input")))
+
+;;;
+
+
+(define (filename->turi webroot filename)
+  (and-let* ((filename)
+             ;; Check that 'filename' is under webroot
+             ((string-prefix? webroot filename))
+             (filename (string-drop filename (+ 1 (string-length webroot)))))
+    (url-encode  filename)))
+
+(test-group
+ "filename->turi"
+ (test
+  "some.mp3"
+  (filename->turi "/tmp/foo" "/tmp/foo/some.mp3"))
+ (test
+  "name%20with%20spaces.mp3"
+  (filename->turi "/tmp/foo" "/tmp/foo/name with spaces.mp3"))
+ (test
+  #f
+  (filename->turi "/tmp/foo" "/outside/webroot")))
+
+;;;
+
+(define (container? res)
+  (cond ((alist-ref 'type res) => (lambda (x) (string-prefix? "container." x)))
+        (else #f)))
+
+(define (track? res)
+  (cond ((alist-ref 'type res) => (lambda (x) (equal? "item.audioItem.musicTrack" x)))
+        (else #f)))
+
+(define (response-container res)
+  (let ((get (cut alist-ref <> res)))
+    `((title .    ,(get 'name))
+      (uri . ,(return-url "/catalog/usb/browse?id=" (get 'id))))))
+
+(define (response-track res)
+  (let* ((get (cut alist-ref <> res))
+         (host (uri-host (request-uri (current-request))))
+         (turi (filename->turi *web-root* (get 'filename))))
+
+    (or turi
+        (print (conc "ERROR: file '" (get 'filename) "' is not served by server (serving: '" *web-root* "')")))
+
+    `((title . ,(get 'title))
+      (subtitle . ,(get 'artist))
+      (type . "usb")
+      (duration . ,(transform-duration (get 'duration)))
+      (turi . ,(conc "tr://" host ":" *static-server-port* "/" turi)))))
+
+(define (transform-result res)
+  (cond ((container? res) (response-container res))
+        ((track? res) (response-track res))
+        (else (print "unknown row: " res))))
+
+(define (transform-results res)
+  (map transform-result res))
+
+;;;
 
 (define ((%make-results proc) q limit offset)
-  (let ((result (result-transform (proc q limit offset))))
+  (let* ((ret (or (proc q limit offset)
+                  ;; No database found, usb stick not present
+                  (response-unavailable)))
+         (result (transform-results ret)))
     (make-search-result limit offset (length result) result)))
 
-(define-handler /v1/browse/usb (argumentize (%make-results browse) 'q '(limit "10") '(offset "0")))
-(define-handler /v1/catalog/usb (argumentize (%make-results search) 'q '(limit "10") '(offset "0")))
+(define-handler /v1/catalog/usb
+  (lambda () `((search .  #( ((title . "Search") (uri . ,(return-url "/catalog/usb/browse")))))
+          (preload . #( ((title . "Browse") (uri . ,(return-url "/catalog/usb/search"))))))))
 
+(define-handler /v1/catalog/usb/browse (argumentize (%make-results browse) '(id "1") '(limit "10") '(offset "0")))
+(define-handler /v1/catalog/usb/search (argumentize (%make-results search) 'q '(limit "10") '(offset "0")))
 
