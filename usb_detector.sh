64b78d402c0d938d0b9e31d7a93ce357821c4dcb
Peder Refsnes <peder.refsnes@gmail.com>
2015-04-24 16:17:19 +0200

    usb: adds script for detecting usb mounts
    the script uses vdc to monitor vold (sort of udev for android)
    
    on mount: run mediascanner, creating a db of usb contents
    on umount: delete the database
    
    TODO: check for presens of usb stick during startup

---
+++ usb_detector.sh
@@ -0,0 +1,36 @@
+#!/system/bin/csi -s
+;; -*- scheme -*-
+
+(use posix matchable srfi-13)
+
+(define (mount-msg? msg)
+  (string-contains msg "to 4 (Mounted)"))
+
+(define (umount-msg? msg)
+  (string-contains msg "to 0 (No-Media)"))
+
+(define (create-media-db)
+  (print "creating mediadb")
+  (system "mediascanner -s /mnt/udisk -d /data" ))
+
+(define (delete-media-db)
+  (print "deleting mediadb")
+  (system "rm /data/files.db"))
+
+(define (process-line line)
+  (match line
+    (#!eof #f)
+    ((? mount-msg?)  (create-media-db))
+    ((? umount-msg?) (delete-media-db))
+    ;; Ignore, but keep going
+    (else #t)))
+
+(with-input-from-pipe
+ "vdc monitor"
+ (lambda ()
+   (print "Connected to Vold")
+   (let loop ()
+     (let ((line (read-line)))
+       (if (process-line line)
+           (loop))))))
+
