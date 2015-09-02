4caf7b87a7fd1628a8a9238b609e2540ca27d737
Peder Refsnes <peder.refsnes@gmail.com>
2015-09-02 12:10:52 +0200

    rest icon: adds DELETE method
    DELETE /player/icon resets the icon to an 'unset' state.

--- rest-icon.scm
+++ rest-icon.scm
@@ -2,6 +2,8 @@
 
 (import chicken scheme data-structures)
 
+(use intarweb spiffy)
+
 ;; local imports
 (import restlib store)
 
@@ -9,13 +11,18 @@
                                    (conc "speaker-icon" "-"
                                          (rest-server-port)))))
 
+(define empty-value `((icon . 0)
+                      (name . "")))
+
 (define-handler /v1/player/icon
   (lambda ()
+    (if (eq? 'DELETE (request-method (current-request)))
+        (current-json empty-value))
+
     (if (current-json)
         ;; TODO: maybe validate that incoming json has field 'icon'
         ;; with an integer value
         (begin (speaker-store (current-json))
                '((status . "ok")))
         (or (speaker-store)
-            `((icon . 0)
-              (name . "")))))))
+            empty-value)))))
