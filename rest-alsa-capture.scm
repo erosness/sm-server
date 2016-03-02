4b5fe97fee60219f94126b4710a59258b86da1f5
Kristian Lein-Mathisen <kristianlein@gmail.com>
2016-03-02 16:34:05 +0100

    refac minor: rename variable
    bt->turi was leftovers from when bluetooth lived there. now it's generic
    for any ALSA capture unit.

--- rest-alsa-capture.scm
+++ rest-alsa-capture.scm
@@ -9,21 +9,21 @@
 
 (import turi)
 
-(define-turi-adapter bt->turi "alsa"
+(define-turi-adapter alsa->turi "alsa"
   (lambda (params)
     `((url . ,(alist-ref 'd params))
       (format . "alsa"))))
 
 (define-handler /v1/catalog/line-in
   (lambda ()
-    `((turi . ,(bt->turi '((d . "default:CARD=imxaudiotr2"))))
+    `((turi . ,(alsa->turi '((d . "default:CARD=imxaudiotr2"))))
       (title . "Line-In")
       (type . "line-in")
       (image . "http://www.futureshop.ca/multimedia/Products/500x500/102/10200/10200642.jpg"))))
 
 (define-handler /v1/catalog/toslink
   (lambda ()
-    `((turi . ,(bt->turi '((d . "default:CARD=imxspdif"))))
+    `((turi . ,(alsa->turi '((d . "default:CARD=imxspdif"))))
       (title . "Toslink")
       (type . "toslink")
       (image . "http://www.thonet-vander.com/img/icon/spdif.png"))))
