39d4c811d995859fb201c22ca8047645c9dc635b
Andries Kruithof <andries.kruithof@tandbergradio.com>
2017-08-31 18:21:30 +0200

    Fixed ip reading on maestro, needs verification on solo

--- rest-icon.scm
+++ rest-icon.scm
@@ -2,32 +2,39 @@
 
 (import chicken scheme data-structures srfi-1)
 
-(use test restlib clojurian-syntax ports
-     srfi-18 extras posix srfi-1 srfi-13
-     medea matchable irregex matchable medea matchable irregex  intarweb spiffy
+(use intarweb spiffy
+     medea matchable irregex ports clojurian-syntax restlib srfi-18 extras
+     posix srfi-1 srfi-13
      (only posix with-input-from-pipe))
 
 ;; local imports
 (import restlib store)
 
-(define uid_leader 
+(define mac 
+  (conc 
+    (read (open-input-file "/sys/fsl_otp/HW_OCOTP_MAC1")) 
+    ":" 
+    (read (open-input-file "/sys/fsl_otp/HW_OCOTP_MAC0"))))
+
+(define find_ip_leader
   (irregex-match-substring
    (irregex-search
-    '(: "inet " (=> ip (or "192" "10") ;; assuming your LAN has this ip
-                    (= 1 "." (** 1 3 numeric))
-                    (= 1 ".42")
-                    (= 1 "." (** 1 3 numeric))
-                                    ) )
-    (with-input-from-pipe "ip a|grep inet" read-string))
+    '(: "inet " (=> ip (or"192" "10")
+		    (= 1 "." (** 1 3 numeric))
+		    (= 1 ".42")
+		    (= 1 "." (** 1 3 numeric))
+		    ))
+    (conc (with-input-from-pipe "ip a|grep inet" read-string) " inet 192.168.42.1"))
    'ip))
 
 (define speaker-store (make-store (string->symbol
                                    (conc "speaker-icon" "-"
                                          (rest-server-port)))))
 
-(define empty-value `((icon . 0)
+(define empty-value `((uid_leader . "192.168.42.1")
+		      (icon . 0)
                       (name . "")
-                      (uid_leader  . ,uid_leader)
+                      (uid  . ,mac)
                       (type . "M")) )
 
 (define-handler /v1/player/icon
@@ -44,5 +51,5 @@
                                      void)
                '((status . "ok")))
         (if (speaker-store)
-            (alist-cons 'uid_leader uid_leader (alist-delete 'uid_leader (speaker-store)) )
+            (alist-cons 'uid_leader find_ip_leader 'uid mac (alist-delete 'uid (speaker-store)) )
             empty-value)))))
