770456681b24a54ca9fc81265093b9cabb3742ea
Erik Rosness <erik.rosness@tandbergradio.com>
2018-11-26 12:40:40 +0100

    Remove code related to nrepl port.

--- cube-server-program.scm
+++ cube-server-program.scm
@@ -38,7 +38,6 @@
    ;; (define dns-sd-unregister!/pq      (register-pq-with-icon-store nickname port "cube"))
 
    (define server-thread (start-rest-server!))
-   (start-nrepl)
 
    (if (assoc 'n opts)
        (thread-join! server-thread)
