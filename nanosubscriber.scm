bb3c7ccf2ac0cbd409952f6a5e314666f5328a4b
Erik Rosness <erik.rosness@tandbergradio.com>
2019-03-26 14:05:49 +0100

    Get rid of obsolete prototype files.

--- nanosubscriber.scm
+++
@@ -1,8 +0,0 @@
-;; nanoif test client.
-(use posix matchable test string-utils)
-(import nanoif)
-
-(define (parse-response resp)
-  (print "Response from gstplay: " resp))
-
-(define request (make-nano-if "ipc:///data/nanomessage/test.pub"))
