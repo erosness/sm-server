2d604a2734da08a8708037c96d0886df441117e6
Erik Rosness <erik.rosness@tandbergradio.com>
2019-03-27 09:36:24 +0100

    Remove some obsolete debug printouts.

--- nanoif/nanoif.scm
+++ nanoif/nanoif.scm
@@ -30,7 +30,6 @@
   (let ((rec (%make-nano-if
               (make-req-if req-addr)
               #f)))
-      (print "Did etablish both interfaces")
       rec))
 
 (define (make-nano-if req-addr pub-addr)
@@ -38,7 +37,6 @@
   (let ((rec (%make-nano-if
               (make-req-if req-addr)
               (make-sub-if pub-addr))))
-      (print "Did etablish both interfaces")
       rec))
 
 ;; ==== Request interface starts here
@@ -91,8 +89,6 @@
 
 ;; Create the subscriber part of the interface
 (define (make-sub-if pub-addr)
-  (print "At make-sub-if:")
-  (print "At make-sub-if:" pub-addr)
   (if pub-addr
   (let* ((socket (make-sub-socket pub-addr))
         (rec (%make-sub-if
@@ -140,7 +136,6 @@
             "Nano-work-thread")))
             (thread-start! nano-thread)
             (let ((result (thread-join! nano-thread 4 #f)))
-              (print "Got result from thread:" result)
               result)))
 
 ;; Do the request operation.
