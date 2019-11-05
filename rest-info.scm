105064e90c9aada8a7e662c13c53674baa445cfc
Erik Rosness <erik@rosness.no>
2019-11-05 16:11:29 +0100

    Add definitions for the remaining units

--- rest-info.scm
+++ rest-info.scm
@@ -23,12 +23,18 @@
   (match uid
     ("4c:cc:6a:d7:c1:78\n" '#("doorlock" "this-is-host"))
     ("b8:27:eb:33:47:88\n" '#("doorlock" "doorbell-out" "door-motor"))
+    ("b8:27:eb:4c:26:ae\n" '#("presence" "doorbell-in" "shade-motor"))
+    ("b8:27:eb:6f:22:ad\n" '#("presence" "floor"))
+    ("b8:27:eb:c6:f9:04\n" '#("presence" "floor" "shade-motor"))
     (_ #("no-service"))))
 
 (define (unit-name uid)
   (match uid
     ("4c:cc:6a:d7:c1:78\n" "host")
     ("b8:27:eb:33:47:88\n" "pi2")
+    ("b8:27:eb:4c:26:ae\n" "pi3a")
+    ("b8:27:eb:6f:22:ad\n" "pi3b")
+    ("b8:27:eb:c6:f9:04\n" "pi3c")
     (_ #("no-name"))))
 
 (define (empty-value)
