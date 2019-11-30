e3e1b067709f7350198ab0c084318e9282ac3503
Erik Rosness <erik@rosness.no>
2019-11-30 10:51:07 +0100

    Fix default typo

--- sm-config.scm
+++ sm-config.scm
@@ -19,12 +19,12 @@
 
 (define (capability)
   (match (uid)
-    ("4c:cc:6a:d7:c1:78" '#("doorlock" "this-is-host" "doorbell-out"))
-    ("b8:27:eb:33:47:88" '#("doorlock" "doorbell-out" "door-motor"))
-    ("b8:27:eb:4c:26:ae" '#("presence" "doorbell-in" "shade-motor"))
-    ("b8:27:eb:6f:22:ad" '#("presence" "floor"  "doorbell-out"))
-    ("b8:27:eb:c6:f9:04" '#("presence" "floor" "shade-motor"))
-    (_ #("no-service"))))
+    ("4c:cc:6a:d7:c1:78" '("doorlock" "this-is-host" "doorbell-out"))
+    ("b8:27:eb:33:47:88" '("doorlock" "doorbell-out" "door-motor"))
+    ("b8:27:eb:4c:26:ae" '("presence" "doorbell-in" "shade-motor"))
+    ("b8:27:eb:6f:22:ad" '("presence" "floor"  "doorbell-out"))
+    ("b8:27:eb:c6:f9:04" '("presence" "floor" "shade-motor"))
+    (_ ("no-service"))))
 
 (define (unit-name)
   (match (uid)
