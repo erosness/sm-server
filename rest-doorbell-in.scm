a557303080399281f1597d6846130d41debd21c0
Erik Rosness <erik@rosness.no>
2019-12-09 15:25:09 +0100

    Cleanup & set correct pin numbers

--- rest-doorbell-in.scm
+++ rest-doorbell-in.scm
@@ -1,5 +1,5 @@
-;; Server for out-door part of the doorbell.
-(module rest-doorbell-out ()
+;; Server for in-door part of the doorbell.
+(module rest-doorbell-in ()
 
 (import chicken scheme data-structures srfi-1 intarweb spiffy srfi-69 data-structures)
 
@@ -11,47 +11,25 @@
 
 
 (define doorbell-out-store (make-store (string->symbol
-                             (conc "doorbell-out" "-"
+                             (conc "doorbell-in" "-"
                                (rest-server-port)))))
 
-(define answer? (make-gpio-input 22))
-(define answer! (make-gpio-output 22))
-(define unlock? (make-gpio-input 24))
-(define unlock! (make-gpio-output 24))
+;; Parts implemented as physical connection via GPIO
+(define phy-unlock-button?  (make-gpio-input 10))
+(define phy-connect-button? (make-gpio-input 12))
 
-(define default-settings
-  `((has-lock  . #t)
-    (has-video . #f)))
+;; Parts implemented as SW modules
 
-(define-handler /v1/sm/doorbell-out/settings
-  (lambda ()
-    (if (eq? 'DELETE (request-method (current-request)))
-        (current-json default-settings))
-    (if (current-json)
-        ;; TODO: maybe validate that incoming json has field 'icon'
-        ;; with an integer value
-        (begin (doorbell-out-store (current-json))
-               '((status . "ok")))
-        (if (doorbell-out-store)
-          (doorbell-out-store)
-		      default-settings))))
+;; Common return status definition
+(define (status?)
+  `((fid . ,(fid (uid) "doorbell-out"))
+    (unlock-button  . ,(phy-unlock-button?))
+    (connect-button . ,(phy-connect-button?))))
 
+;; The pure GET status (no PUT)
 (define-handler /v1/sm/doorbell-in/status
-  (lambda ()
-    `((fid . ,(fid (uid) "doorbell-in"))
-      (answer . ,(answer?))
-      (unlock . ,(unlock?)))))
+  (lambda () (status?)))
 
-
-(define-handler /v1/sm/doorbell-in/bell
-  (lambda ()
-    `((active . ,(answer?)))))
-
-(define-handler /v1/sm/doorbell-in/lock
-  (lambda ()
-    (if (current-json)
-      (answer! (alist-ref 'answer (current-json))))
-    `((answer . ,(answer?)))))
-
+;; PUT definitions
 
 )
