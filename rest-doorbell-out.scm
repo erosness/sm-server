f01dd8b4440c5b793e1b28ac197c8cb34e81fde9
Erik Rosness <erik@rosness.no>
2019-12-10 10:59:43 +0100

    Add age to doorbell nad local poll (0.5 sec interval)

--- rest-doorbell-out.scm
+++ rest-doorbell-out.scm
@@ -1,10 +1,11 @@
 ;; Server for out-door part of the doorbell.
 (module rest-doorbell-out ()
 
-(import chicken scheme data-structures srfi-1 intarweb spiffy srfi-69 data-structures)
+(import chicken scheme data-structures srfi-1 intarweb spiffy
+        srfi-69 srfi-18 data-structures clojurian-syntax)
 
 ;; local imports
-(import restlib store sm-config gpio)
+(use restlib store sm-config gpio looper)
 
 (define (fid uid cap)
   (string-hash (conc uid cap)))
@@ -14,18 +15,40 @@
                              (conc "doorbell-out" "-"
                                (rest-server-port)))))
 
+(define (invert lvl)
+  (lambda ()(if (= (lvl) 0) 1 0)))
+
 ;; Parts implemented as physical connection via GPIO
-(define phy-doorbell? (make-gpio-input   3))
-(define phy-unlock?   (make-gpio-input   8))
-(define phy-unlock!   (make-gpio-output  7))
-(define phy-dooropen? (make-gpio-input   5))
+(define phy-doorbell? (invert(make-gpio-input   3)))
+(define phy-unlock?          (make-gpio-input   8))
+(define phy-unlock!          (make-gpio-output  7))
+(define phy-dooropen?        (make-gpio-input   5))
 
 ;; Parts implemented as SW modules
+(define doorbell-time #f)
+
+(define (doorbell-body)
+  (if (not (= 0 (phy-doorbell?)))
+    (set! doorbell-time (time->seconds (current-time)))))
+
+(define doorbell-thread
+  (thread-start!
+    (->>
+      doorbell-body
+      (loop/interval 0.5)
+      (loop)
+      ((flip make-thread) "Doorbell-thread"))))
+
+(define (doorbell-age)
+  (if doorbell-time
+    (round(max 0 (- (time->seconds(current-time)) doorbell-time)))
+    -1 ))
 
 ;; Common return status definition
 (define (status?)
   `((fid . ,(fid (uid) "doorbell-out"))
     (doorbell . ,(phy-doorbell?))
+    (doorbellAge . ,(doorbell-age))
     (unlock . ,(phy-unlock?))
     (dooropen . ,(phy-dooropen?))))
 
@@ -34,6 +57,12 @@
   (lambda () (status?)))
 
 ;; PUT definitions
+(define-handler /v1/sm/doorbell-out/doorbell-age
+  (lambda ()
+    (if (current-json)
+      (set! doorbell-time #f))
+    (status?)))
+
 (define-handler /v1/sm/doorbell-out/lock
   (lambda ()
     (if (current-json)
