c253fb749183ceea9977e8754d26668f3a3d4378
Erik Rosness <erik@rosness.no>
2019-12-17 09:52:25 +0100

    Replace fixed debug address with address from caller.

--- linphone/wrap-linphone.c
+++ linphone/wrap-linphone.c
@@ -6,7 +6,6 @@ void state_changed(void* lc, void* call, int cstate, char* msg);
 LinphoneCoreVTable vtable={0};
 LinphoneCore *lc;
 LinphoneCall *call=NULL;
-const char *dest="sip:pi@10.0.1.104";
 
 LinphoneCore *lph_create() {
   vtable.call_state_changed=state_changed;
@@ -20,18 +19,18 @@ void lph_core_destroy(LinphoneCore* arg_lc) {
   lc = NULL;
 }
 
-LinphoneCall *lph_call(LinphoneCore* arg_lc, char* arg_dest){
-  printf("arg_dest: %s", arg_dest);
+LinphoneCall *lph_call(LinphoneCore* arg_lc, char* dest){
+  printf("arg_dest: %s", dest);
   printf("LC: here: %p got: %p\n", lc, arg_lc  );
   /*
    Place an outgoing call
   */
-  call=linphone_core_invite(lc,dest);
+  call=linphone_core_invite(lc, dest);
   if (call==NULL){
-    printf("Could not place call to %s\n",dest);
+    printf("Could not place call to %s\n", dest);
     return (void*)-1;
   }else{
-    printf("Call to %s is in progress...",dest);
+    printf("Call to %s is in progress...", dest);
   }
   linphone_call_ref(call);
   printf("Call status: ");
