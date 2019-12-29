0f274df7fa273b7d45308c93a1461cec33df6bd3
Erik Rosness <erik@rosness.no>
2019-12-29 09:40:54 +0100

    Adding thread for async operation. Debug included.

--- led-matrix.scm
+++ led-matrix.scm
@@ -2,7 +2,7 @@
 
 (module led-matrix *
 
-(import chicken scheme extras srfi-18)
+(import chicken scheme extras srfi-18 looper clojurian-syntax data-structures)
 
 (define (red) (write-byte #x00) (write-byte #xF0))
 (define (green) (write-byte #x00) (write-byte #x0F))
@@ -32,5 +32,14 @@
       (with-exception-handler (lambda (x) (k #f))
                               (lambda ()
                                 (display-list led-list time))))))
+
+
+(define (animate-thread led-list #!optional (time 0.2) (repeat #t))
+  (thread-start!
+    (->>
+      (lambda () (print "loop:" (animate-led led-list time)) #f)
+      (loop/interval 0.5)
+      (loop)
+      ((flip make-thread) "Animate-thread"))))
 
 )
