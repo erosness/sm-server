da667a6a9129ac20bd777cb205705e4ab1716027
Erik Rosness <erik@rosness.no>
2020-01-27 16:52:28 +0100

    Strech active unlock, i.e. ugnore unlock de-activete for 8 seconds after being activated. Hack to avoid two conflicting doorbell-in units make havoc of function. The active unlocker will win.

--- rest-doorbell-out.scm
+++ rest-doorbell-out.scm
@@ -25,6 +25,18 @@
 (define phy-unlock!          (make-gpio-output  7))
 (define phy-dooropen?        (make-gpio-input   5))
 
+(define lock-time 0)
+
+(define (unlock! val)
+(if (= 0 val)
+  (let ((off-age (- (time->seconds (current-time)) lock-time )))
+    (print "off-age=" off-age)
+    (if (< 8 off-age)
+      (phy-unlock! 0)))
+  (begin
+   (set! lock-time (time->seconds (current-time)))
+   (phy-unlock! 1))))
+
 ;; Parts implemented as SW modules
 (define doorbell-time #f)
 
@@ -68,7 +80,7 @@
 (define-handler /v1/sm/doorbell-out/lock
   (lambda ()
     (if (current-json)
-      (phy-unlock! (alist-ref 'unlock (current-json))))
+      (unlock! (alist-ref 'unlock (current-json))))
     (status?)))
 
 (define-handler /v1/sm/doorbell-out/voice
